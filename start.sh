fb2c8c6acd0341d13e1bcbe1d4c6a96b3514ae9d
George Paulose <georgepaulose@everledger.io>
2020-04-23 19:24:25 +0530

    Modified

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 export GO111MODULE=on
 ERR="$(go build -o app ./cmd/... 2>&1)"
 
-[  -z "$ERR" ] && ./app --port=8080 || echo ERROR: "${ERR}"; exit 1
+[  -z "$ERR" ] && ./app --port=9000 || echo ERROR: "${ERR}"; exit 1
