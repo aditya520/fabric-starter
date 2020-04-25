7b553d9fefb070a52a6ba5eae26059918188aaa5
George Paulose <georgepaulose@everledger.io>
2020-04-25 21:30:22 +0530

    Added binaries fetch

--- start.sh
+++ start.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 export GO111MODULE=on
 export PYTHONWARNINGS="ignore"
+
+if ! [ -d "./bin" ]; then
+    echo "Binaries dont exist..."
+    echo "Pulling latest binaries"
+    curl -o binaries.sh -sL https://bit.ly/2ysbOFE
+    chmod a+x binaries.sh
+    ./binaries.sh -s -d
+    rm -rf binaries.sh
+    rm -rf ./config
+fi
+
 ERR="$(go build -o app ./cmd/... 2>&1)"
 
 [  -z "$ERR" ] && ./app --port=9000 || echo ERROR: "${ERR}"; exit 1
