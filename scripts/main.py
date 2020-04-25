5ed82d5b587db236265e3fe77d3ae3589f5adc9f
George Paulose <georgepaulose@everledger.io>
2020-04-25 20:25:55 +0530

    Added generate artifacts to main script

--- scripts/main.py
+++ scripts/main.py
@@ -3,6 +3,7 @@ import create_docker_base as base
 import create_docker_cli as cli
 import create_configtx as configtx
 import create_crypto_config as crypto
+import generate_artifcats as gen_art
 import json
 import os
 import sys
@@ -18,8 +19,6 @@ sys.stdout.flush()
 with open(path) as f:
     jsonData = json.load(f)
 
-
-
 ## Crypto Config ##
 crypto.create_crypto(jsonData)
 
@@ -33,3 +32,5 @@ base.create_base(jsonData)
 ## Docker CLI ##
 cli.create_docker_compose_cli(jsonData)
 
+## Artifacts ##
+gen_art
