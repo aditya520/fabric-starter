07ef07b5f4a6577ee165357c37919d73b2a98617
George Paulose <georgepaulose@everledger.io>
2020-04-25 15:29:40 +0530

    Added ordr base

--- scripts/create_docker_base.py
+++ scripts/create_docker_base.py
@@ -1,25 +1,56 @@
-import yaml
+import pyaml
 import json
 import copy
 
-yaml.SafeDumper.ignore_aliases = lambda *args: True
+#pyaml.SafeDumper.ignore_aliases = lambda *args: True
 
 def create_base(jsonData):
-    #orderer_init_port = 7050
     
+    peerService = create_peer_base(jsonData)
+    
+    ordrService = create_ordr_base(jsonData)
 
-    with open("./docker-files/boilerplate_files/docker-compose-base-boilerplate.yaml") as f:
-        base_doc = yaml.load(f)
-
-    services = create_peer_base(base_doc, jsonData)
-    base_doc["services"].update(services)
-
+    services = {
+        "services": {**ordrService,**peerService}
+    }
+    
     with open("./docker-files/final_files/base/docker-compose-base.yaml", "w+") as f:
-        yaml.safe_dump(base_doc, f)
+        pyaml.dump(services, f, vspacing=[2, 1])
 
 
+def create_ordr_base(jsonData):
+    orderer_init_port = 7050
+    #TODO: Parse from input json
+    network_name = ["byfn"]
+    
+    services = {}
+    
+    for ordr in jsonData["organizations"]["ordererOrg"]["url"]:
+       services[ordr] = {
+           "extends":{
+               "file": "base/peer-base.yaml",
+               "service": "orderer-base"
+           },
+           "environment":[
+               "ORDERER_GENERAL_LISTENPORT="+str(orderer_init_port)
+           ],
+           "container_name": ordr,
+           "networks": network_name,
+           "volumes":[
+               "./channel-artifacts/genesis.block:/var/hyperledger/orderer/orderer.genesis.block",
+               "./crypto-config/ordererOrganizations/example.com/orderers/"+ordr+"/msp:/var/hyperledger/orderer/msp",
+               "./crypto-config/ordererOrganizations/example.com/orderers/"+ordr+"/tls/:/var/hyperledger/orderer/tls",
+               ordr+":/var/hyperledger/production/orderer"
+           ],
+           "ports":[
+               str(orderer_init_port)+":"+str(orderer_init_port)
+           ]
+       }
+       orderer_init_port = orderer_init_port + 1000
+    
+    return services      
 
-def create_peer_base(base_doc, jsonData):
+def create_peer_base(jsonData):
     peer_name = "peer"
     peer_init_port = 7051
 
@@ -28,20 +59,16 @@ def create_peer_base(base_doc, jsonData):
     org_mspID = []
     org_peer_count = []
 
-    services = copy.deepcopy(base_doc["services"])
+    services = {}
     
     for i in range(0, len(jsonData["organizations"]["peerOrgs"])):
         org = jsonData["organizations"]["peerOrgs"][i]["url"]
         org_mspID.append(jsonData["organizations"]["peerOrgs"][i]["mspID"])
         org_peer_count.append(int(jsonData["organizations"]["peerOrgs"][i]["count"]))
-        print("orgPeerCount: ",org_peer_count)
         bootstrap_peer_count = 0
         if i != 0:
             bootstrap_peer_count = sum(org_peer_count) - org_peer_count[i]
         
-        print("Bootstrap: ",bootstrap_peer_count)
-
-
         for peer in range(0, int(jsonData["organizations"]["peerOrgs"][i]["count"])):
             orgName = peer_name + str(peer) + "." + org
             orgNameWithPort = orgName + ":" + str(peer_init_port)
@@ -65,8 +92,6 @@ def create_peer_base(base_doc, jsonData):
                     "CORE_PEER_CHAINCODEADDRESS=" + orgName + ":" + str(peer_init_port + 1), "CORE_PEER_CHAINCODELISTENADDRESS=0.0.0.0:" + str(peer_init_port + 1),
                     "CORE_PEER_GOSSIP_BOOTSTRAP=" + "peer1." + jsonData["organizations"]["peerOrgs"][i]["url"] + ":" + str(peer_init_port + 1000), "CORE_PEER_GOSSIP_EXTERNALENDPOINT=" + orgNameWithPort,"CORE_PEER_LOCALMSPID=" + org_mspID[i]]
                 else:
-                    print("IN with ",i, " org")
-                    print("Bootstrapped: ",org_final_name_withPort[bootstrap_peer_count])
                     services[orgName]["environment"] = [
                     "CORE_PEER_ID=" + orgName, "CORE_PEER_ADDRESS=" + orgNameWithPort, "CORE_PEER_LISTENADDRESS=0.0.0.0:" + str(peer_init_port),
                     "CORE_PEER_CHAINCODEADDRESS=" + orgName + ":" + str(peer_init_port + 1), "CORE_PEER_CHAINCODELISTENADDRESS=0.0.0.0:" + str(peer_init_port + 1),
