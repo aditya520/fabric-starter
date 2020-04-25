2cdc7ae6b0839038bc041013c9dfa6f6a7070a30
George Paulose <georgepaulose@everledger.io>
2020-04-25 14:41:25 +0530

    Modularised and updated algo for bootstrap

--- scripts/create_docker_base.py
+++ scripts/create_docker_base.py
@@ -1,41 +1,46 @@
 import yaml
 import json
-
-
+import copy
 
 yaml.SafeDumper.ignore_aliases = lambda *args: True
 
-
-
 def create_base(jsonData):
-    orderer_init_port = 7050
-    peer_init_port = 7051
+    #orderer_init_port = 7050
+    
 
     with open("./docker-files/boilerplate_files/docker-compose-base-boilerplate.yaml") as f:
         base_doc = yaml.load(f)
 
-    # print (base_doc)
+    services = create_peer_base(base_doc, jsonData)
+    base_doc["services"].update(services)
 
-    # Let's make base doc for peers #### (Wish me luck)
+    with open("./docker-files/final_files/base/docker-compose-base.yaml", "w+") as f:
+        yaml.safe_dump(base_doc, f)
 
+
+
+def create_peer_base(base_doc, jsonData):
     peer_name = "peer"
-    # TODO: Update to take value from json
-    network_name = ["byfn"]
+    peer_init_port = 7051
 
     org_final_name = []
     org_final_name_withPort = []
     org_mspID = []
     org_peer_count = []
 
-    services = base_doc["services"]
+    services = copy.deepcopy(base_doc["services"])
     
     for i in range(0, len(jsonData["organizations"]["peerOrgs"])):
         org = jsonData["organizations"]["peerOrgs"][i]["url"]
         org_mspID.append(jsonData["organizations"]["peerOrgs"][i]["mspID"])
         org_peer_count.append(int(jsonData["organizations"]["peerOrgs"][i]["count"]))
+        print("orgPeerCount: ",org_peer_count)
         bootstrap_peer_count = 0
         if i != 0:
-            bootstrap_peer_count = org_peer_count[i-1]
+            bootstrap_peer_count = sum(org_peer_count) - org_peer_count[i]
+        
+        print("Bootstrap: ",bootstrap_peer_count)
+
 
         for peer in range(0, int(jsonData["organizations"]["peerOrgs"][i]["count"])):
             orgName = peer_name + str(peer) + "." + org
@@ -60,6 +65,8 @@ def create_base(jsonData):
                     "CORE_PEER_CHAINCODEADDRESS=" + orgName + ":" + str(peer_init_port + 1), "CORE_PEER_CHAINCODELISTENADDRESS=0.0.0.0:" + str(peer_init_port + 1),
                     "CORE_PEER_GOSSIP_BOOTSTRAP=" + "peer1." + jsonData["organizations"]["peerOrgs"][i]["url"] + ":" + str(peer_init_port + 1000), "CORE_PEER_GOSSIP_EXTERNALENDPOINT=" + orgNameWithPort,"CORE_PEER_LOCALMSPID=" + org_mspID[i]]
                 else:
+                    print("IN with ",i, " org")
+                    print("Bootstrapped: ",org_final_name_withPort[bootstrap_peer_count])
                     services[orgName]["environment"] = [
                     "CORE_PEER_ID=" + orgName, "CORE_PEER_ADDRESS=" + orgNameWithPort, "CORE_PEER_LISTENADDRESS=0.0.0.0:" + str(peer_init_port),
                     "CORE_PEER_CHAINCODEADDRESS=" + orgName + ":" + str(peer_init_port + 1), "CORE_PEER_CHAINCODELISTENADDRESS=0.0.0.0:" + str(peer_init_port + 1),
@@ -77,11 +84,4 @@ def create_base(jsonData):
 
             peer_init_port = peer_init_port + 1000
 
-
-    # TODO: Algorithm for PEER_GOSSIP_BOOTSTRAP
-
-    base_doc["services"].update(services)
-
-    with open("./docker-files/final_files/base/docker-compose-base.yaml", "w+") as f:
-        yaml.safe_dump(base_doc, f)
-
+    return services
