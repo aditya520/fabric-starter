1dde2dd706699bc14c2ae8618c79bbcd49636c3b
George Paulose <georgepaulose@everledger.io>
2020-04-25 20:25:17 +0530

    Updated to remove python warnings

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 export GO111MODULE=on
+export PYTHONWARNINGS="ignore"
 ERR="$(go build -o app ./cmd/... 2>&1)"
 
 [  -z "$ERR" ] && ./app --port=9000 || echo ERROR: "${ERR}"; exit 1
