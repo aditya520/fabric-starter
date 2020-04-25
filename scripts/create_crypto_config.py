d11bc4ed20362c08ca050edc3b2c9be68e809613
George Paulose <georgepaulose@everledger.io>
2020-04-26 01:04:42 +0530

    Modularised

--- scripts/create_crypto_config.py
+++ scripts/create_crypto_config.py
@@ -15,35 +15,41 @@ def create_crypto(jsonData):
     with open("./network/template/crypto-config-template.yaml") as f:
         crypto_doc = yaml.load(f)
 
+    peerOrgsArr = create_crypto_orgs(jsonData,crypto_doc)
+    ordrArr = create_crypto_ordr(jsonData,crypto_doc)
 
+    crypto_doc["PeerOrgs"]=peerOrgsArr
+    crypto_doc["OrdererOrgs"][0]["Specs"] = ordrArr
+
+    with open("./network/crypto-config.yaml", "w+") as f:
+            pyaml.dump(crypto_doc, f, vspacing=[2, 1])
+
+
+def create_crypto_orgs(jsonData, crypto_doc):
     n_user = 1 # Hardocded for now
-
-
     peerOrgsArr = []
-    ordrArr = []
     for org in jsonData["organizations"]["peerOrgs"]:
-        peerOrgs = copy.deepcopy(crypto_doc["PeerOrgs"][0])
-        
-        org_name = org["name"]
-        org_peer_count = org["count"]
-        org_url = org["url"]
+       peerOrgs = copy.deepcopy(crypto_doc["PeerOrgs"][0])
+       
+       org_name = org["name"]
+       org_peer_count = org["count"]
+       org_url = org["url"]
 
-        peerOrgs["Name"] = org_name.lower().capitalize()
-        peerOrgs["Domain"] = org_url
-        peerOrgs["EnableNodeOUs"] = True
-        peerOrgs["Template"]["Count"] = org_peer_count
-        peerOrgs["Users"]["Count"] = n_user
-        peerOrgsArr.append(peerOrgs)
+       peerOrgs["Name"] = org_name.lower().capitalize()
+       peerOrgs["Domain"] = org_url
+       peerOrgs["EnableNodeOUs"] = True
+       peerOrgs["Template"]["Count"] = org_peer_count
+       peerOrgs["Users"]["Count"] = n_user
+       peerOrgsArr.append(peerOrgs)
+        
+    return peerOrgsArr
+    
+def create_crypto_ordr(jsonData, crypto_doc):
+    ordrArr = []
 
     for ordr in jsonData["organizations"]["ordererOrg"]["url"]:
         ordrSpec = copy.deepcopy(crypto_doc["OrdererOrgs"][0]["Specs"][0])
         ordrSpec["Hostname"] = ordr.split(".")[0]
         ordrArr.append(ordrSpec)
-
-
-    crypto_doc["PeerOrgs"]=peerOrgsArr
-
-    crypto_doc["OrdererOrgs"][0]["Specs"] = ordrArr
-
-    with open("./network/crypto-config.yaml", "w+") as f:
-            pyaml.dump(crypto_doc, f, vspacing=[2, 1])
+    
+    return ordrArr
