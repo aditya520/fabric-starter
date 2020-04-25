50f2580c752a0c10b53b14e4c9a1bd9b39b78e4f
George Paulose <georgepaulose@everledger.io>
2020-04-26 01:03:07 +0530

    Cleanup added

--- start.sh
+++ start.sh
@@ -12,6 +12,10 @@ if ! [ -d "./bin" ]; then
     rm -rf ./config
 fi
 
+
+rm -rf ./network/crypto-config
+rm -rf ./network/channel-artifacts/*
+
 ERR="$(go build -o app ./cmd/... 2>&1)"
 
 [  -z "$ERR" ] && ./app --port=9000 || echo ERROR: "${ERR}"; exit 1
